69f602d3553e1df3c037c882b69c225dfccb98a4
Greg-Olson <olson.gregory714@gmail.com>
2021-12-06 17:26:19 -0700

    Updating after 5. Bug with name column

--- app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
@@ -6,12 +6,8 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TaskDao {
 
-    //Room now makes this a compile time error instead of a run tim error.
     @Query("SELECT * FROM task_table")
-    //A flow is an asynchonous stream of data
-    //So when this is called we get a stream of tasks
-    //Flow can only be used in kotlin coroutine
-    fun getTask():Flow<List<Task>>
+    fun getTasks(): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
